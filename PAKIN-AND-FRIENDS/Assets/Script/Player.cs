653a08368ceff29771599d795b2156a00d5a4c1f
Pakin18888888 <238475616+takzakung9-hue@users.noreply.github.com>
2025-11-25 08:38:10 +0700

    Add

---
+++ PAKIN-AND-FRIENDS/Assets/Script/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Inventory inventoryl;
+    // public List<Skill> activeSkills;
+    public int senity;
+    public int stamina;
+
+    public void Interact(){
+
+    }
+    
+}
