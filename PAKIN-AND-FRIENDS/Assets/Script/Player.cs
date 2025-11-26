fd70c5c2971808a33796be69d2eae99c91976eb7
Pakin18888888 <238475616+takzakung9-hue@users.noreply.github.com>
2025-11-26 12:04:21 +0700

    Update Player.cs

--- PAKIN-AND-FRIENDS/Assets/Script/Player.cs
+++ PAKIN-AND-FRIENDS/Assets/Script/Player.cs
@@ -1,13 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour, IInteractable
 {
-    public Inventory inventoryl;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    public Inventory inventory;
     // public List<Skill> activeSkills;
     public int senity;
     public int stamina;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 currentMoveInput = context.ReadValue<Vector2>();
+        
+        animator.SetBool("IsWalking", currentMoveInput.magnitude > 0);
+
+        if (context.canceled)
+        {
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        moveInput = currentMoveInput;
+        
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+    }
 
     public void Interact(IInteractable obj){
 
@@ -17,5 +51,4 @@ public class Player : MonoBehaviour, IInteractable
     {
         
     }
-    
 }
