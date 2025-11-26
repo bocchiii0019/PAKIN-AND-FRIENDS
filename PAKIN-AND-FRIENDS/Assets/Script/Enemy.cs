8e2e2057df17417878260929e60a7d54ddfd2708
bocchiii0019 <jaruwantreechot@gmail.com>
2025-11-26 19:50:25 +0700

    Implement enemy movement towards target based on detection range

--- PAKIN-AND-FRIENDS/Assets/Script/Enemy.cs
+++ PAKIN-AND-FRIENDS/Assets/Script/Enemy.cs
@@ -6,6 +6,10 @@ public class Enemy : MonoBehaviour
 {
     // Sta
     // rt is called before the first frame update
+    public Transform target;
+    private string EnemyID;
+    public float detectionRange = 2f;
+    public float moveSpeed = 3f;
     
     void Start()
     {
@@ -15,6 +19,14 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(transform.position, target.position);
+        if (distance > detectionRange)
+        {
+            // ทิศทางวิ่งเข้าหา
+            Vector3 direction = (target.position - transform.position).normalized;
+
+            // ขยับตัวละครไปตามทิศทาง
+            transform.position += direction * moveSpeed * Time.deltaTime;
+        }
     }
 }
